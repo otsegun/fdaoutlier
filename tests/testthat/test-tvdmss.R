12cc01d509b602ed17ffbde673a393a40813b988
Oluwasegun Ojo <oluwasegun.ojo@imdea.org>
2020-09-24 17:23:40 +0200

    add more test for tvd mss

--- tests/testthat/test-tvdmss.R
+++ tests/testthat/test-tvdmss.R
@@ -9,12 +9,18 @@ test_that("tvd_mss finds outliers produces correct results", {
 
 })
 
-
 test_that("tvd_mss behaves well when outliers are not found", {
   dt <- sim_data1$data[c(1:3, 5:9), 1:7]
   tvdmssobj <- tvd_mss(dt)
 
   expect_null(tvdmssobj$shape_outliers)
   expect_null(tvdmssobj$magnitude_outliers)
+
+})
+
+test_that("tvd_mss behaves well when shape outliers are found", {
+  dt <- sim_data1$data
+  tvdmssobj <- tvd_mss(dt)
+  expect_equal(tvdmssobj$outliers, 79)
 
 })
